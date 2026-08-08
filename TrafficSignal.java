a434bc78d263a4ccc652ee20bebfb473c5d7cf71
hu0517-u <hu0517@srmist.edu.in.com>
2026-08-08 15:37:01 +0530

    addTrafficSignal

---
+++ TrafficSignal.java
@@ -0,0 +1,30 @@
+public class TrafficSignal {
+
+    static void findLongestStreak(String signalLog) {
+
+        char longestChar = signalLog.charAt(0);
+        int longest = 1;
+        int current = 1;
+
+        for (int i = 1; i < signalLog.length(); i++) {
+
+            if (signalLog.charAt(i) == signalLog.charAt(i - 1)) {
+                current++;
+            } else {
+                current = 1;
+            }
+
+            if (current > longest) {
+                longest = current;
+                longestChar = signalLog.charAt(i);
+            }
+        }
+
+        System.out.println("Longest Streak: '" + longestChar +
+                "' repeated " + longest + " times");
+    }
+
+    public static void main(String[] args) {
+        findLongestStreak("RRGGGYRR");
+    }
+}
